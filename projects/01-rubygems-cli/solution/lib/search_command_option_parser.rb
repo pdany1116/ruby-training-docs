67036e4d92cab35f0195aff20ea19b248fe27311
Daniel Popa <danypdaniel@gmail.com>
2022-04-02 11:24:42 +0300

    Refactor search option parser

--- projects/01-rubygems-cli/solution/lib/search_command_option_parser.rb
+++ projects/01-rubygems-cli/solution/lib/search_command_option_parser.rb
@@ -2,17 +2,15 @@ require "optparse"
 
 class SearchCommandOptionParser
   class << self
-    def parse(options)
-      options = { :most_downloads_first => false, :license => nil }
+    def parse()
+      options = { most_downloads_first: false, license: nil }
       
       OptionParser.new do |opts|
-        opts.on("--most-downloads-first", 
-          "Order gems starting with the downloaded first.") do
+        opts.on("--most-downloads-first") do
             options[:most_downloads_first] = true
         end
     
-        opts.on("--license LICENSE", 
-          "Filter gems by specified license.") do |license|
+        opts.on("--license LICENSE") do |license|
             options[:license] = license
         end
       end.parse!(into: options)
