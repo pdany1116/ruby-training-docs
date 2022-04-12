a47ceefcbc87bba22e720bd3a963d34db8e01114
Daniel Popa <danypdaniel@gmail.com>
2022-04-13 02:21:31 +0300

    Refactor api

--- projects/02-meme-generator-api/solution/lib/api.rb
+++ projects/02-meme-generator-api/solution/lib/api.rb
@@ -1,46 +1,32 @@
 require "sinatra/base"
 require "json"
 require "pry"
-require "down"
 require "./lib/memefier"
 require "./lib/meme_body_parser"
-require "./lib/errors/meme_body_error"
+require "./lib/image_downloader"
 
 class API < Sinatra::Application
-  TMP_PATH = "./tmp/"
-
   post "/generate" do
     body = MemeBodyParser.parse(request.body.read)
-    create_tmp_directory
-    image_path = TMP_PATH + body.file_name
-    Down.download(body.uri, destination: image_path)
-    Memefier.memefy(image_path, body.text)
+
+    ImageDownloader.create.download(body.uri, body.file_name)
+    Memefier.memefy(body.file_name, body.text)
 
     redirect "/meme/#{body.file_name}", 303
 
-  rescue Down::Error, URI::Error
-    [400, "Invalid url in request body!"]
-  rescue MemeBodyError => e
+  rescue MemeBodyError, ImageDownloaderError => e
     [400, "#{e}"]
-  rescue JSON::ParserError
-    [400, "Invalid request body syntax!"]
   rescue
     [500, "An unknown internal error occured!"]
   end
 
   get "/meme/:file" do
-    send_file(TMP_PATH + params[:file])
+    send_file("./tmp/" + params[:file])
   end
 
   class << self
     def run
       run!
     end
-  end
-
-  private
-
-  def create_tmp_directory
-    Dir.mkdir(TMP_PATH) unless Dir.exists?(TMP_PATH)
   end
 end
